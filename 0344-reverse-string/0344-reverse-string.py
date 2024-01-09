398488d512ffa85d5a86d8e0664afeb4ef6063a2
dony1220 <96913965+dony1220@users.noreply.github.com>
2024-01-09 23:58:27 +0900

    Update 0344-reverse-string.py

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,4 +4,14 @@ class Solution:
         s[:] = s[::-1]
         
 
-        
+
+#2. reverse string
+# def reverseString(s):
+#     s = s[::-1]
+#     return s
+
+# s = ['h', 'e', 'l']
+
+# print(reverseString(s))
+#이렇게 하면 왜 안되는거지??
+#--> 쳐보니까 공간복잡도 때매 안된다고 함..
