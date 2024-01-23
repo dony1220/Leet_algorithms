2a562de1a2acc990e68485fa211197d15bc9a36a
dony1220 <96913965+dony1220@users.noreply.github.com>
2024-01-23 19:48:01 +0900

    Time: 914 ms (5.03%), Space: 24.9 MB (29.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,14 +3,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         result = []
-        s = 1
-        for i in range(0, len(nums)):
-            result.append(s)
-            s *= nums[i]
-        s = 1
-        for i in range(len(nums)-1, -1, -1):
-            result[i] *= s
-            s*=nums[i]
-        return result
+        #조합으로 못푸나? combination
+        #빈리스트에 담을거랑, 초반 곱, 나중 곱 이렇게 나눠서 ㄱ
+        f = [1]
+        e = [1]
         
+        for i in range(len(nums)-1):
+            f.append(f[-1] * nums[i])
+        for i in range(len(nums)-1, 0, -1):
+            e.insert(0, e[0] * nums[i])
+        for i in range(len(nums)):
+            result.append(f[i] * e[i])
+        return result
+
+#o(n)은 아니지만 2중 for문으로도 가는ㅇ하지않나?
+#ex)
+
+#
+# p=[1] * len(nums) [1로 다 초기화 시켜놓고]
+
+# for i in range(len(nums)):
+#     for j in range(len(nums)):
+#         if i!=j:
+#             p[i] *= nums[j]
+#     return p
         
